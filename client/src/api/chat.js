336517e4bd1cbd86d3c6704fc1c2b9979fddd1be
Arnab Majumder <arnabmajumdar399@gmail.com>
2025-01-13 19:07:54 +0530

    cleaning up unnecessary codes

--- client/src/api/chat.js
+++ client/src/api/chat.js
@@ -1,31 +1,3 @@
-// import axios from 'axios';
-
-// const API_BASE_URL = 'http://localhost:3000/api';
-
-// export const generateOptions = async (query) => {
-//   try {
-//     const response = await axios.post(`${API_BASE_URL}/generate-options`, { query });
-//     return response.data.options;
-//   } catch (error) {
-//     console.error('Error generating options:', error);
-//     throw new Error('Failed to generate options');
-//   }
-// };
-
-// export const generateDetailedResponse = async (query, selectedOptions) => {
-//   try {
-//     const response = await axios.post(`${API_BASE_URL}/generate-detailed-response`, {
-//       query,
-//       selectedOptions,
-//     });
-//     return response.data.response;
-//   } catch (error) {
-//     console.error('Error generating detailed response:', error);
-//     throw new Error('Failed to generate detailed response');
-//   }
-// };
-
-
 import axios from "axios"
 
 const API_BASE_URL = "http://localhost:3000/api"
